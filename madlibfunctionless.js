50eefa879ba9a7f9b2536e127736bfdc744ab031
cshelt99 <31927310+cshelt99@users.noreply.github.com>
2017-10-13 08:06:32 -0400

    Madlib Functionless

---
+++ madlibfunctionless.js
@@ -0,0 +1,22 @@
+var sentence = [];
+var replacements = [];
+var index = 0;
+
+filler(sentence,"Next Word: ");
+filler(sentence,"Next Word: ");
+filler(replacements, "Next Positon: ");
+
+
+	index = 0;
+do {
+	console.log(index);
+		myArray[index] = prompt(myPrompt+index+ ":");
+	index++;
+}
+	while(myArray[index - 1] != "^");
+	myArray.pop();
+	console.log(myArray);
+	
+		
+	}
+filler(sent,"Word:");
